71347e3dbf09fcdccfb00bb5e480bad923803a0b
Jim M <jmoro@pop-os.localdomain>
2021-09-17 17:45:11 -0500

    added boot2

---
+++ boot2.c
@@ -0,0 +1,17 @@
+/**************************************
+ *Jim Moroney              09.17.2021 *
+ *boot2.c                             *
+ *HW1                         CSC4100 *
+ **************************************/
+
+ //Functions written in C
+ void println(char *string);
+ void k_clearscr();
+ //Functions written in asm
+ void k_print(char *string, int string_length, int row, int col);
+ void k_scroll();
+
+ int main()
+ {
+     
+ }
---
+++ boot2.s
@@ -0,0 +1,4 @@
+;boot2.s
+;Jim Moroney 09.17.2021
+;CSC4100 - Operating Systems with Dr. Rogers
+;Part of HW1
